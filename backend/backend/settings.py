0b7aa81546b1efefd05b094e4f3304ed1e3f5866
Johann Cepeda <profiledx16@hotmail.com>
2025-02-28 19:51:48 -0500

    feat: adding django-rest-framwork

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -21,8 +21,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'rest_framework',
     'corsheaders',
+    'rest_framework',
     'properties',
 ]
 
